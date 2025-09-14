63c27777ecd4c9627c072461abec188f3784c410
roy-ayush22 <ayush30085@gmail.com>
2025-09-14 23:47:05 +0530

    commit

---
+++ variables/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "initial"
+    fmt.Println(a)
+
+    var b,c = 1,2
+    fmt.Println(b,c)
+
+    cd := 23
+    fmt.Println(cd)
+
+    f := "borderline"
+    fmt.Println(f)
+}
